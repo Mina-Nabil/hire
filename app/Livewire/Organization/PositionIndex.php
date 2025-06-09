1ca79c3ef15a7de30f0756b38677f76e19d15acc
Mina Nabil <mina9492@hotmail.com>
2025-06-09 17:41:01 +0300

    fixes

--- app/Livewire/Organization/PositionIndex.php
+++ app/Livewire/Organization/PositionIndex.php
@@ -222,7 +222,9 @@ class PositionIndex extends Component
     public function openEditPositionSec($id)
     {
         $position = Position::find($id);
+        
         $this->employees = Employee::current()->unemployed()->get();
+        $this->employees->push($position->employee);
 
         $this->positionId = $position->id;
         $this->positionName = $position->name;

f422c1996490a06a0d899ac9900b1f499519a939
Mina Nabil <mina9492@hotmail.com>
2025-06-22 21:04:12 +0300

    fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: [
+            'iclock/*',
+        ]);
+
         $middleware->alias([
             'type' => UserIsType::class,
             'auth-attendance-device' => AuthorizeAttendanceDevice::class,

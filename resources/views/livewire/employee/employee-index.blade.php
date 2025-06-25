6028bf655a7443a0f5da82d828606e0bb898dede
Mina Nabil <mina9492@hotmail.com>
2025-06-25 11:16:38 +0300

    adding employee management using EXCEL

--- resources/views/livewire/employee/employee-index.blade.php
+++ resources/views/livewire/employee/employee-index.blade.php
@@ -62,7 +62,7 @@
                                         {{ __('Name') }}
                                     </th>
                                     <th scope="col" class=" table-th ">
-                                        {{ __('Email') }}
+                                        {{ __('Employment') }}
                                     </th>
                                     <th scope="col" class=" table-th ">
                                         {{ __('Phone') }}
@@ -85,7 +85,7 @@
                                             {{ $employee->name }}
                                         </td>
                                         <td class="table-td">
-                                            {{ $employee->email }}
+                                            {{ $employee->employment_date ? $employee->employment_date->format('d/m/Y') : '-' }}
                                         </td>
                                         <td class="table-td">
                                             {{ $employee->phone }}

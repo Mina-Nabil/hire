11567adc8add44c837613bb0909af6f8e11f5d7e
Mina Nabil <mina9492@hotmail.com>
2025-06-29 15:33:46 +0300

    fixes

--- resources/views/livewire/recruitment/vacancy-show.blade.php
+++ resources/views/livewire/recruitment/vacancy-show.blade.php
@@ -5,36 +5,38 @@
             <h4>
                 <b>{{ $vacancy->position->name }} Vacancy</b>
             </h4>
-            <div class="dropdown relative">
-                <button class="btn inline-flex justify-center btn-dark items-center btn-sm" type="button"
-                    id="darkDropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                    Actions
-                    <iconify-icon class="text-xl ltr:ml-2 rtl:mr-2" icon="ic:round-keyboard-arrow-down"></iconify-icon>
-                </button>
-                <ul
-                    class=" dropdown-menu min-w-max absolute text-sm text-slate-700 dark:text-white hidden bg-white dark:bg-slate-700 shadow
+            @can('delete', $vacancy)
+                <div class="dropdown relative">
+                    <button class="btn inline-flex justify-center btn-dark items-center btn-sm" type="button"
+                        id="darkDropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                        Actions
+                        <iconify-icon class="text-xl ltr:ml-2 rtl:mr-2" icon="ic:round-keyboard-arrow-down"></iconify-icon>
+                    </button>
+                    <ul
+                        class=" dropdown-menu min-w-max absolute text-sm text-slate-700 dark:text-white hidden bg-white dark:bg-slate-700 shadow
                                 z-[2] float-left overflow-hidden list-none text-left rounded-lg mt-1 m-0 bg-clip-padding border-none">
 
-                    <li wire:click='$dispatch("showConfirmation", {
+                        <li wire:click='$dispatch("showConfirmation", {
                         title: "Delete Applicant",
                         message: "Are you sure you want to delete this applicant?",
                         color: "danger",
                         callback: "deleteApplicant",
                     })'
-                        class="text-slate-600 dark:text-white block font-Inter font-normal px-4 py-2 hover:bg-slate-100 dark:hover:bg-slate-600
+                            class="text-slate-600 dark:text-white block font-Inter font-normal px-4 py-2 hover:bg-slate-100 dark:hover:bg-slate-600
                                         dark:hover:text-white cursor-pointer">
-                        Delete Vacancy
-                    </li>
+                            Delete Vacancy
+                        </li>
 
-                </ul>
-            </div>
+                    </ul>
+                </div>
+            @endcan
         </div>
 
     </div>
 
 
 
-    <div class="card-body flex flex-col col-span-2" >
+    <div class="card-body flex flex-col col-span-2">
         <div class="card-text h-full">
 
             <div class="flex" wire:ignore>
@@ -57,16 +59,17 @@
                             <iconify-icon class="mr-1" icon="healthicons:city-worker"></iconify-icon>
                             Applicants & Interviews </a>
                     </li>
-                    <li class="nav-item" role="presentation" wire:click="changeSection('manage')">
-                        <a href="#tabs-messages-withIcon"
-                            class="nav-link w-full flex items-center font-medium text-sm font-Inter leading-tight capitalize border-x-0 border-t-0 border-b border-transparent px-4 pb-2 my-2 hover:border-transparent focus:border-transparent  @if ($section === 'manage') active @endif dark:text-slate-300"
-                            id="tabs-messages-withIcon-tab" data-bs-toggle="pill"
-                            data-bs-target="#tabs-messages-withIcon" role="tab"
-                            aria-controls="tabs-messages-withIcon" aria-selected="false">
-                            <iconify-icon class="mr-1" icon="mdi:settings"></iconify-icon>
-                            Manage Vacancy </a>
-                    </li>
-
+                    @can('update', $vacancy)
+                        <li class="nav-item" role="presentation" wire:click="changeSection('manage')">
+                            <a href="#tabs-messages-withIcon"
+                                class="nav-link w-full flex items-center font-medium text-sm font-Inter leading-tight capitalize border-x-0 border-t-0 border-b border-transparent px-4 pb-2 my-2 hover:border-transparent focus:border-transparent  @if ($section === 'manage') active @endif dark:text-slate-300"
+                                id="tabs-messages-withIcon-tab" data-bs-toggle="pill"
+                                data-bs-target="#tabs-messages-withIcon" role="tab"
+                                aria-controls="tabs-messages-withIcon" aria-selected="false">
+                                <iconify-icon class="mr-1" icon="mdi:settings"></iconify-icon>
+                                Manage Vacancy </a>
+                        </li>
+                    @endcan
                 </ul>
                 <div>
                     <h4>
@@ -92,14 +95,14 @@
                         @include('livewire.recruitment.partials.vacancy-applicants')
                     </div>
                 @endif
-
-                @if ($section === 'manage')
-                    <div class="tab-pane fade show active" id="tabs-messages-withIcon" role="tabpanel"
-                        aria-labelledby="tabs-messages-withIcon-tab">
-                        @include('livewire.recruitment.partials.vacancy-manage')
-                    </div>
-                @endif
-
+                @can('update', $vacancy)
+                    @if ($section === 'manage')
+                        <div class="tab-pane fade show active" id="tabs-messages-withIcon" role="tabpanel"
+                            aria-labelledby="tabs-messages-withIcon-tab">
+                            @include('livewire.recruitment.partials.vacancy-manage')
+                        </div>
+                    @endif
+                @endcan
             </div>
         </div>
     </div>

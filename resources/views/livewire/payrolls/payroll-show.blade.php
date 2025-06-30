3920adb6abd49fb8e7ce4ba8d7db51ed3e596aa5
Mina Nabil <mina9492@hotmail.com>
2025-06-30 17:17:52 +0300

    fixes

--- resources/views/livewire/payrolls/payroll-show.blade.php
+++ resources/views/livewire/payrolls/payroll-show.blade.php
@@ -396,7 +396,7 @@
                                                     {{ $attendance->hours ? $attendance->hours . 'h' : 'N/A' }}
                                                     @if ($attendance->penalized_hours > 0)
                                                         <span>
-                                                            - ({{ $attendance->penalized_hours > 1.0 ? number_format($attendance->penalized_hours * 60, 0) . " min" : number_format($attendance->penalized_hours, 0) . " h" }})
+                                                            - ({{ $attendance->penalized_hours > 1.0 ? number_format($attendance->penalized_hours, 0) . " h" : number_format($attendance->penalized_hours * 60, 0) . " min" }})
                                                         </span>
                                                     @endif
                                                 </span>

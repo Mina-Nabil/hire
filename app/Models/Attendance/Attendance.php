101e89156f7969860368fa6a45a0b12ae983c9b8
Mina Nabil <mina9492@hotmail.com>
2025-06-30 16:28:15 +0300

    fixes

--- app/Models/Attendance/Attendance.php
+++ app/Models/Attendance/Attendance.php
@@ -169,7 +169,7 @@ class Attendance extends Model
                 ];
                 continue;
             }
-            if (!$employee->benefitConfiguration)  throw new AppException('Employee ' . $employee->name . ' has no benefit configuration');
+            if (!$employee->benefitConfiguration || !$employee->benefitConfiguration->attendance_calculation)  continue;
 
             $attendanceType = $employee->benefitConfiguration->attendance_calculation;
 

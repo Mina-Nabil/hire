828f706f02e994b2ee4fc4868504e3f2d85b858c
Mina Nabil <mina9492@hotmail.com>
2025-07-13 12:41:17 +0300

    adding import/export

--- app/Http/Controllers/ZKDeviceController.php
+++ app/Http/Controllers/ZKDeviceController.php
@@ -264,7 +264,10 @@ class ZKDeviceController extends Controller
                 'employee_name' => $employee->name,
                 'device_id' => $employee->info->device_id ?? 'null'
             ]);
-
+            $punch_time = Carbon::parse($timestamp);
+            if(env('BASMA_TIME_ADJUSTMENT')){
+                $punch_time = $punch_time->addHours(env('BASMA_TIME_ADJUSTMENT'));
+            }
             $punches_to_insert[] = [
                 'employee_id' => $employee->id,
                 'punch_time' => Carbon::parse($timestamp),

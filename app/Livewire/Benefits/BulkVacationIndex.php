f032350f9fb043356d0f6be97ba7b80b03c6f763
Mina Nabil <mina9492@hotmail.com>
2025-07-28 23:02:27 +0300

    fixes

--- app/Livewire/Benefits/BulkVacationIndex.php
+++ app/Livewire/Benefits/BulkVacationIndex.php
@@ -212,6 +212,7 @@ class BulkVacationIndex extends Component
                 break;
             case VacationDetail::TYPE_MONTHLY:
                 $appliedVacations = AppliedVacation::getAppliedHours($employeeId, Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth(), $benefit['name'], $benefit['id'] ?? null);
+                Log::info($benefit['name']);
                 Log::info($appliedVacations);
                 Log::info($value);
                 $this->employeesData[$employeeId]['vacationBenefits'][$key]['current_balance'] = round($value - $appliedVacations, 2);
